5c3f9cc58964a1a7f04135b7b875c45877920502
Gabriel Negrão Silva <itsg_negrao@hotmail.com>
2017-07-01 13:02:21 -0300

    Update 05-03-MalhaOBJ.cpp

--- src/05-03-MalhaOBJ.cpp
+++ src/05-03-MalhaOBJ.cpp
@@ -4,362 +4,391 @@
 #include <GL/freeglut.h>
 
 //#define DEBUG 1
-// Vari·veis para controles de navegaÁ„o
+// Vari√°veis para controles de navega√ß√£o
 GLfloat angle, fAspect;
 GLfloat rotX, rotY, rotX_ini, rotY_ini;
 GLfloat obsX, obsY, obsZ, obsX_ini, obsY_ini, obsZ_ini;
 int x_ini,y_ini,bot;
 
 // Apontador para objeto
-OBJ *pedra;
+OBJ *nuvens;
 OBJ *objeto2;
 
-// FunÁ„o respons·vel pela especificaÁ„o dos par‚metros de iluminaÁ„o
+// Fun√ß√£o respons√°vel pela especifica√ß√£o dos par√¢metros de ilumina√ß√£o
 void DefineIluminacao (void)
 {
-	GLfloat luzAmbiente[4]={0.2,0.2,0.2,1.0};
-	GLfloat luzDifusa[4]={1.0,1.0,1.0,1.0};	   	// "cor"
-	GLfloat luzEspecular[4]={1.0, 1.0, 1.0, 1.0};	// "brilho"
-	GLfloat posicaoLuz[4]={0.0, 10.0, 100.0, 1.0};
+    GLfloat luzAmbiente[4]={0.2,0.2,0.2,1.0};
+    GLfloat luzDifusa[4]={1.0,1.0,1.0,1.0};           // "cor"
+    GLfloat luzEspecular[4]={1.0, 1.0, 1.0, 1.0};    // "brilho"
+    GLfloat posicaoLuz[4]={0.0, 10.0, 100.0, 1.0};
 
-	// Capacidade de brilho do material
-	GLfloat especularidade[4]={1.0,1.0,1.0,1.0};
-	GLint especMaterial = 60;
+    // Capacidade de brilho do material
+    GLfloat especularidade[4]={1.0,1.0,1.0,1.0};
+    GLint especMaterial = 60;
 
 
-	// Define a reflet‚ncia do material
-	glMaterialfv(GL_FRONT,GL_SPECULAR, especularidade);
-	// Define a concentraÁ„o do brilho
-	glMateriali(GL_FRONT,GL_SHININESS,especMaterial);
+    // Define a reflet√¢ncia do material
+    glMaterialfv(GL_FRONT,GL_SPECULAR, especularidade);
+    // Define a concentra√ß√£o do brilho
+    glMateriali(GL_FRONT,GL_SHININESS,especMaterial);
 
-	// Ativa o uso da luz ambiente
-	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, luzAmbiente);
+    // Ativa o uso da luz ambiente
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, luzAmbiente);
 
-	// Define os par‚metros da luz de n˙mero 0
-	glLightfv(GL_LIGHT0, GL_AMBIENT, luzAmbiente);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, luzDifusa );
-	glLightfv(GL_LIGHT0, GL_SPECULAR, luzEspecular );
-	glLightfv(GL_LIGHT0, GL_POSITION, posicaoLuz );
+    // Define os par√¢metros da luz de n√∫mero 0
+    glLightfv(GL_LIGHT0, GL_AMBIENT, luzAmbiente);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, luzDifusa );
+    glLightfv(GL_LIGHT0, GL_SPECULAR, luzEspecular );
+    glLightfv(GL_LIGHT0, GL_POSITION, posicaoLuz );
 }
 
 void DesenhaChao(void){
 //Lado multicolorido - FRENTE
     glBegin(GL_POLYGON);
-    glVertex3f( -400, -200, -400);       // P1
-    glVertex3f( -400,  200, -400);       // P2
-    glVertex3f(  400,  200, -400);       // P3
-    glVertex3f(  400, -200, -400);
+    glVertex3f( -200, -200, -800);       // P1
+    glVertex3f( -200,  200, -800);       // P2
+    glVertex3f(  200,  200, -800);       // P3
+    glVertex3f(  200, -200, -800);
     glEnd();
 
   // Lado branco - TRASEIRA
     glBegin(GL_POLYGON);
-    glVertex3f(  400, -200, 400 );
-    glVertex3f(  400,  200, 400 );
-    glVertex3f( -400,  200, 400 );
-    glVertex3f( -400, -200, 400 );
+    glVertex3f(  200, -200, 800 );
+    glVertex3f(  200,  200, 800 );
+    glVertex3f( -200,  200, 800 );
+    glVertex3f( -200, -200, 800 );
     glEnd();
 
   // Lado roxo - DIREITA
     glBegin(GL_POLYGON);
-    glVertex3f( 400, -200, -400 );
-    glVertex3f( 400,  200, -400 );
-    glVertex3f( 400,  200,  400 );
-    glVertex3f( 400, -200,  400 );
+    glVertex3f( 200, -200, -800 );
+    glVertex3f( 200,  200, -800 );
+    glVertex3f( 200,  200,  800 );
+    glVertex3f( 200, -200,  800 );
     glEnd();
 
   // Lado verde - ESQUERDA
     glBegin(GL_POLYGON);
-    glVertex3f( -400, -200, 400 );
-    glVertex3f( -400,  200, 400 );
-    glVertex3f( -400,  200, -400);
-    glVertex3f( -400, -200, -400 );
+    glVertex3f( -200, -200, 800 );
+    glVertex3f( -200,  200, 800 );
+    glVertex3f( -200,  200, -800);
+    glVertex3f( -200, -200, -800 );
     glEnd();
 
   // Lado vermelho - BASE
     glBegin(GL_POLYGON);
-    glVertex3f(  400, -200, -400 );
-    glVertex3f(  400, -200,  400 );
-    glVertex3f( -400, -200, 400 );
-    glVertex3f( -400, -200, -400 );
+    glVertex3f(  200, -200, -800 );
+    glVertex3f(  200, -200,  800 );
+    glVertex3f( -200, -200, 800 );
+    glVertex3f( -200, -200, -800 );
     glEnd();
 
       // Lado azul - TOPO
     glColor3f(0.0f, 0.5f, 0.0f);
     glBegin(GL_POLYGON);
-    glVertex3f(  400,  200,  400 );
-    glVertex3f(  400,  200, -400 );
-    glVertex3f( -400,  200, -400 );
-    glVertex3f( -400,  200,  400 );
+    glVertex3f(  200,  200,  800 );
+    glVertex3f(  200,  200, -800 );
+    glVertex3f( -200,  200, -800 );
+    glVertex3f( -200,  200,  800 );
     glEnd();
 }
 
-// FunÁ„o callback de redesenho da janela de visualizaÁ„o
+    float posicaoZ_dragao=150.0;
+    float posicaoX_dragao=0.0;
+    float inc = -0.5;
+
+void Timer(void){
+    if(posicaoZ_dragao <= -150.0 ) posicaoZ_dragao = 150.0;
+    posicaoZ_dragao = posicaoZ_dragao + inc;
+    glutPostRedisplay();
+
+}
+// Fun√ß√£o callback de redesenho da janela de visualiza√ß√£o
 void Desenha(void)
 {
-	// Limpa a janela de visualizaÁ„o com a cor
-	// de fundo definida previamente
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glMatrixMode(GL_MODELVIEW);
+    // Limpa a janela de visualiza√ß√£o com a cor
+    // de fundo definida previamente
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
 
-	// Altera a cor do desenho para rosa
-	glColor3f(0.0f, 0.5f, 0.0f);
+    // Altera a cor do desenho para rosa
+    glColor3f(0.0f, 0.5f, 0.0f);
 
-	glPushMatrix();
+    glPushMatrix();
     glRotatef(rotX,1,0,0);
-	glRotatef(rotY,0,1,0);
+    glRotatef(rotY,0,1,0);
 
-    //desenha ch„o
+    //desenha ch√£o
     glTranslatef(0.0, -210.0, 0.0);
     glColor3f(0.5,0.25,0.20);
     DesenhaChao();
     glTranslatef(0.0, 210.0, 0.0);
 
-	// Desenha o objeto 3D lido do arquivo com a cor corrente
+    // Desenha o objeto 3D lido do arquivo com a cor corrente
     glColor3f(1.0f, 0.0f, 0.0f);
 
-	glTranslatef(-10.0, 0.0, 0.0);
+    glTranslatef(-10.0, 0.0, 0.0);
     glScalef(5.0,5.0,5.0);
-	DesenhaObjeto(pedra);
-	glTranslatef(-10.0, 0.0, 0.0);
-	DesenhaObjeto(pedra);
-	glTranslatef(20.0, 0.0, 0.0);
-	glScalef(0.09,0.09,0.09);
-	DesenhaObjeto(objeto2);
 
+    DesenhaObjeto(nuvens);
+
+    glTranslatef(-10.0, 0.0, 0.0);
+
+    DesenhaObjeto(nuvens);
+
+    glTranslatef(15+posicaoX_dragao, 2.0, posicaoZ_dragao);
+
+    glScalef(0.4,0.4,0.4);
+    glRotatef(180,0,1,0);
+
+    DesenhaObjeto(objeto2);
+    Timer();
+    //glutTimerFunc(40,Timer, 1);
 
     glPopMatrix();
 
-	// Executa os comandos OpenGL
-	glutSwapBuffers();
+    // Executa os comandos OpenGL
+    glutSwapBuffers();
 }
 
-// FunÁ„o usada para especificar a posiÁ„o do observador virtual
+// Fun√ß√£o usada para especificar a posi√ß√£o do observador virtual
 void PosicionaObservador(void){
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();//carrega a matriz de identidade
-    gluLookAt(obsX,obsY,obsZ,//posiÁ„o da c‚mera
-              0.0,0.0,0.0,//para onde a c‚mera aponta
+    gluLookAt(obsX,obsY,obsZ,//posi√ß√£o da c√¢mera
+              0.0,0.0,0.0,//para onde a c√¢mera aponta
               0.0,1.0,0.0);//vetor view-up//
 }
 
-// FunÁ„o usada para especificar o volume de visualizaÁ„o
+// Fun√ß√£o usada para especificar o volume de visualiza√ß√£o
 void EspecificaParametrosVisualizacao(void)
 {
 
-	// Especifica sistema de coordenadas de projeÁ„o
-	glMatrixMode(GL_PROJECTION);
-	// Inicializa sistema de coordenadas de projeÁ„o
-	glLoadIdentity();
+    // Especifica sistema de coordenadas de proje√ß√£o
+    glMatrixMode(GL_PROJECTION);
+    // Inicializa sistema de coordenadas de proje√ß√£o
+    glLoadIdentity();
 
-	// Especifica a projeÁ„o perspectiva(angulo,aspecto,dnear,dfar)
-	gluPerspective(angle,fAspect,0.01,1200);
+    // Especifica a proje√ß√£o perspectiva(angulo,aspecto,dnear,dfar)
+    gluPerspective(angle,fAspect,0.01,1200);
 
-	// Chama as funÁıes que especificam os par‚metros da c‚mera e os par‚metros de iluminaÁ„o
-	PosicionaObservador();
-	DefineIluminacao();
+    // Chama as fun√ß√µes que especificam os par√¢metros da c√¢mera e os par√¢metros de ilumina√ß√£o
+    PosicionaObservador();
+    DefineIluminacao();
 
 }
 
-// FunÁ„o callback chamada quando o tamanho da janela È alterado
+// Fun√ß√£o callback chamada quando o tamanho da janela √© alterado
 void AlteraTamanhoJanela(GLsizei w, GLsizei h)
 {
-	// Para previnir uma divis„o por zero
-	if ( h == 0 ) h = 1;
+    // Para previnir uma divis√£o por zero
+    if ( h == 0 ) h = 1;
 
-	// Especifica as dimensıes da viewport
-	glViewport(0, 0, w, h);
+    // Especifica as dimens√µes da viewport
+    glViewport(0, 0, w, h);
 
-	// Calcula a correÁ„o de aspecto
-	fAspect = (GLfloat)w/(GLfloat)h;
+    // Calcula a corre√ß√£o de aspecto
+    fAspect = (GLfloat)w/(GLfloat)h;
 
-	EspecificaParametrosVisualizacao();
+    EspecificaParametrosVisualizacao();
 }
 
-// FunÁ„o callback chamada para gerenciar eventos de teclas normais (ESC)
+// Fun√ß√£o callback chamada para gerenciar eventos de teclas normais (ESC)
 void Teclas (unsigned char tecla, int x, int y)
 {
-	if(tecla==27) // ESC ?
-	{
-		// Libera memÛria e finaliza programa
-		LiberaObjeto(pedra);
-		LiberaObjeto(objeto2);
-		exit(0);
-	}
-	if(tecla=='m')
-	{
-		if(glIsEnabled(GL_LIGHTING))
-			glDisable(GL_LIGHTING);
-		else
+    if(tecla==27) // ESC ?
+    {
+        // Libera mem√≥ria e finaliza programa
+        LiberaObjeto(nuvens);
+        LiberaObjeto(objeto2);
+        exit(0);
+    }
+    if(tecla=='m')
+    {
+        if(glIsEnabled(GL_LIGHTING))
+            glDisable(GL_LIGHTING);
+        else
             glEnable(GL_LIGHTING);
-	}
-	glutPostRedisplay();
+    }
+    glutPostRedisplay();
 }
 
-// FunÁ„o callback para tratar eventos de teclas especiais
+// Fun√ß√£o callback para tratar eventos de teclas especiais
 void TeclasEspeciais (int tecla, int x, int y)
 {
-	switch (tecla)
-	{
-		case GLUT_KEY_HOME:	if(angle>=10)  angle -=5;
-							break;
-		case GLUT_KEY_END:	if(angle<=150) angle +=5;
-							break;
-	}
-	EspecificaParametrosVisualizacao();
-	glutPostRedisplay();
+    switch (tecla)
+    {
+        case GLUT_KEY_HOME:    if(angle>=10)  angle -=5;
+                            break;
+        case GLUT_KEY_END:    if(angle<=150) angle +=5;
+                            break;
+        case GLUT_KEY_UP:   if(inc>=-2.0) inc-=0.1;
+                            break;
+        case GLUT_KEY_DOWN:  if(inc<-0.2) inc+=0.1;
+                            break;
+        case GLUT_KEY_RIGHT:   if(posicaoX_dragao<=30) posicaoX_dragao+=10;
+                            break;
+        case GLUT_KEY_LEFT:  if(posicaoX_dragao>=-30) posicaoX_dragao-=5;
+                            break;
+    }
+    EspecificaParametrosVisualizacao();
+    glutPostRedisplay();
 }
 
-// FunÁ„o callback para eventos de botıes do mouse
+// Fun√ß√£o callback para eventos de bot√µes do mouse
 void GerenciaMouse(int button, int state, int x, int y)
 {
-	if(state==GLUT_DOWN)
-	{
-		// Salva os par‚metros atuais
-		x_ini = x;
-		y_ini = y;
-		obsX_ini = obsX;
-		obsY_ini = obsY;
-		obsZ_ini = obsZ;
-		rotX_ini = rotX;
-		rotY_ini = rotY;
-		bot = button;
-	}
-	else bot = -1;
+    if(state==GLUT_DOWN)
+    {
+        // Salva os par√¢metros atuais
+        x_ini = x;
+        y_ini = y;
+        obsX_ini = obsX;
+        obsY_ini = obsY;
+        obsZ_ini = obsZ;
+        rotX_ini = rotX;
+        rotY_ini = rotY;
+        bot = button;
+    }
+    else bot = -1;
 }
 
-// FunÁ„o callback para eventos de movimento do mouse
-#define SENS_ROT	5.0
-#define SENS_OBS	15.0
-#define SENS_TRANSL	20.0
+// Fun√ß√£o callback para eventos de movimento do mouse
+#define SENS_ROT    5.0
+#define SENS_OBS    15.0
+#define SENS_TRANSL    20.0
 void GerenciaMovim(int x, int y)
 {
-	// Bot„o esquerdo
-	if(bot==GLUT_LEFT_BUTTON)
-	{
-		// Calcula diferenÁas
-		int deltax = x_ini - x;
-		int deltay = y_ini - y;
-		// E modifica ‚ngulos
-		rotY = rotY_ini - deltax/SENS_ROT;
-		rotX = rotX_ini - deltay/SENS_ROT;
-	}
-	// Bot„o direito (zoom-in e zoom-out)
-	else if(bot==GLUT_RIGHT_BUTTON){
-		// Calcula diferenÁa
-		int deltaz = y_ini - y;
-		// E modifica dist‚ncia do observador
-		obsZ = obsZ_ini + deltaz/SENS_OBS;
-	}
-	// Bot„o do meio
-	else if(bot==GLUT_MIDDLE_BUTTON)
-	{
-		// Calcula diferenÁas
-		int deltax = x_ini - x;
-		int deltay = y_ini - y;
-		// E modifica posiÁıes
-		obsX = obsX_ini + deltax/SENS_TRANSL;
-		obsY = obsY_ini - deltay/SENS_TRANSL;
-	}
-	PosicionaObservador();
-	glutPostRedisplay();
+    // Bot√£o esquerdo
+    if(bot==GLUT_LEFT_BUTTON)
+    {
+        // Calcula diferen√ßas
+        int deltax = x_ini - x;
+        int deltay = y_ini - y;
+        // E modifica √¢ngulos
+        rotY = rotY_ini - deltax/SENS_ROT;
+        rotX = rotX_ini - deltay/SENS_ROT;
+    }
+    // Bot√£o direito (zoom-in e zoom-out)
+    else if(bot==GLUT_RIGHT_BUTTON){
+        // Calcula diferen√ßa
+        int deltaz = y_ini - y;
+        // E modifica dist√¢ncia do observador
+        obsZ = obsZ_ini + deltaz/SENS_OBS;
+    }
+    // Bot√£o do meio
+    else if(bot==GLUT_MIDDLE_BUTTON)
+    {
+        // Calcula diferen√ßas
+        int deltax = x_ini - x;
+        int deltay = y_ini - y;
+        // E modifica posi√ß√µes
+        obsX = obsX_ini + deltax/SENS_TRANSL;
+        obsY = obsY_ini - deltay/SENS_TRANSL;
+    }
+    PosicionaObservador();
+    glutPostRedisplay();
 }
 
-// FunÁ„o respons·vel por inicializar par‚metros e vari·veis
+// Fun√ß√£o respons√°vel por inicializar par√¢metros e vari√°veis
 void Inicializa (void)
 {
-	char nomeArquivo[30];
+    char nomeArquivo[30];
 
-	// Define a cor de fundo da janela de visualizaÁ„o como branca
-	glClearColor(0, 1.0f, 1.0f, 1.0f);
+    // Define a cor de fundo da janela de visualiza√ß√£o como branca
+    glClearColor(0, 1.0f, 1.0f, 1.0f);
 
-	// Habilita a definiÁ„o da cor do material a partir da cor corrente
-	glEnable(GL_COLOR_MATERIAL);
-	//Habilita o uso de iluminaÁ„o
-	glEnable(GL_LIGHTING);
-	// Habilita a luz de n˙mero 0
-	glEnable(GL_LIGHT0);
-	// Habilita o depth-buffering
-	glEnable(GL_DEPTH_TEST);
+    // Habilita a defini√ß√£o da cor do material a partir da cor corrente
+    glEnable(GL_COLOR_MATERIAL);
+    //Habilita o uso de ilumina√ß√£o
+    glEnable(GL_LIGHTING);
+    // Habilita a luz de n√∫mero 0
+    glEnable(GL_LIGHT0);
+    // Habilita o depth-buffering
+    glEnable(GL_DEPTH_TEST);
 
-	// Habilita o modelo de tonalizaÁ„o de Gouraud
-	glShadeModel(GL_SMOOTH);
+    // Habilita o modelo de tonaliza√ß√£o de Gouraud
+    glShadeModel(GL_SMOOTH);
 
-	// Inicializa a vari·vel que especifica o ‚ngulo da projeÁ„o
-	// perspectiva
-	angle=55;
+    // Inicializa a vari√°vel que especifica o √¢ngulo da proje√ß√£o
+    // perspectiva
+    angle=55;
 
-	// Inicializa as vari·veis usadas para alterar a posiÁ„o do
-	// observador virtual
-	obsX = 0;
-	obsY = 0;
-	obsZ = 150;
+    // Inicializa as vari√°veis usadas para alterar a posi√ß√£o do
+    // observador virtual
+    obsX = 0;
+    obsY = 0;
+    obsZ = 1000;
 
-	// LÍ o nome do arquivo e chama a rotina de leitura
-	//printf("Digite o nome do arquivo que contem o modelo 3D: ");
-	//gets(nomeArquivo);
+    // L√™ o nome do arquivo e chama a rotina de leitura
+    //printf("Digite o nome do arquivo que contem o modelo 3D: ");
+    //gets(nomeArquivo);
 
-	// Carrega o objeto 3D
-	objeto2 = CarregaObjeto("obj/MountnN14_obj.obj",true);
-	pedra = CarregaObjeto("monkey.obj",true);
+    // Carrega o objeto 3D
+    objeto2 = CarregaObjeto("obj/KinjaDragern/KinjaDragern.obj",true);
+    nuvens = CarregaObjeto("monkey.obj",true);
 
     printf("Objeto carregado!");
 
-	// E calcula o vetor normal em cada face
-	if(pedra->normais)
-	{
-		// Se j· existirem normais no arquivo, apaga elas
-		free(pedra->normais);
-		pedra->normais_por_vertice = false;
-	}
-	CalculaNormaisPorFace(pedra);
-	if(objeto2->normais)
-	{
-		// Se j· existirem normais no arquivo, apaga elas
-		free(objeto2->normais);
-		objeto2->normais_por_vertice = false;
-	}
-	CalculaNormaisPorFace(objeto2);
+    // E calcula o vetor normal em cada face
+    if(nuvens->normais)
+    {
+        // Se j√° existirem normais no arquivo, apaga elas
+        free(nuvens->normais);
+        nuvens->normais_por_vertice = false;
+    }
+    CalculaNormaisPorFace(nuvens);
+    if(objeto2->normais)
+    {
+        // Se j√° existirem normais no arquivo, apaga elas
+        free(objeto2->normais);
+        objeto2->normais_por_vertice = false;
+    }
+    CalculaNormaisPorFace(objeto2);
 }
 
 // Programa Principal
 int main(int argc, char *argv[])
 {
     glutInit(&argc, argv);
-	// Define do modo de operaÁ„o da GLUT
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    // Define do modo de opera√ß√£o da GLUT
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 
-	// Especifica a posiÁ„o inicial da janela GLUT
-	glutInitWindowPosition(5,5);
+    // Especifica a posi√ß√£o inicial da janela GLUT
+    glutInitWindowPosition(5,5);
 
-	// Especifica o tamanho inicial em pixels da janela GLUT
-	glutInitWindowSize(450,450);
+    // Especifica o tamanho inicial em pixels da janela GLUT
+    glutInitWindowSize(450,450);
 
-	// Cria a janela passando como argumento o tÌtulo da mesma
-	glutCreateWindow("Desenho de um objeto 3D com c·lculo do vetor normal");
+    // Cria a janela passando como argumento o t√≠tulo da mesma
+    glutCreateWindow("Desenho de um objeto 3D com c√°lculo do vetor normal");
 
-	// Registra a funÁ„o callback de redesenho da janela de visualizaÁ„o
-	glutDisplayFunc(Desenha);
+    // Registra a fun√ß√£o callback de redesenho da janela de visualiza√ß√£o
+    glutDisplayFunc(Desenha);
 
-	// Registra a funÁ„o callback de redimensionamento da janela de visualizaÁ„o
-	glutReshapeFunc(AlteraTamanhoJanela);
+    // Registra a fun√ß√£o callback de redimensionamento da janela de visualiza√ß√£o
+    glutReshapeFunc(AlteraTamanhoJanela);
 
-	// Registra a funÁ„o callback para tratamento das teclas normais
-	glutKeyboardFunc (Teclas);
+    // Registra a fun√ß√£o callback para tratamento das teclas normais
+    glutKeyboardFunc (Teclas);
 
-	// Registra a funÁ„o callback para tratamento das teclas especiais
-	glutSpecialFunc (TeclasEspeciais);
+    // Registra a fun√ß√£o callback para tratamento das teclas especiais
+    glutSpecialFunc (TeclasEspeciais);
 
-	// Registra a funÁ„o callback para eventos de botıes do mouse
-	glutMouseFunc(GerenciaMouse);
+    // Registra a fun√ß√£o callback para eventos de bot√µes do mouse
+    glutMouseFunc(GerenciaMouse);
 
-	// Registra a funÁ„o callback para eventos de movimento do mouse
-	glutMotionFunc(GerenciaMovim);
+    // Registra a fun√ß√£o callback para eventos de movimento do mouse
+    glutMotionFunc(GerenciaMovim);
 
-	// Chama a funÁ„o respons·vel por fazer as inicializaÁıes
-	Inicializa();
+    //Fun√ß√£o tempo
 
-	// Inicia o processamento e aguarda interaÁıes do usu·rio
-	glutMainLoop();
 
-	return 0;
+    // Chama a fun√ß√£o respons√°vel por fazer as inicializa√ß√µes
+    Inicializa();
+
+    // Inicia o processamento e aguarda intera√ß√µes do usu√°rio
+    glutMainLoop();
+
+    return 0;
 }
